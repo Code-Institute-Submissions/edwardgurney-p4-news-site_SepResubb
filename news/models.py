b21df21ce91a1b5934729b8d537d0eb4c5bbc43c
edwardgurney <edwardgurney1979@gmail.com>
2021-12-11 13:04:41 +0000

    implement models and migrate to database

--- news/models.py
+++ news/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="news_posts")
+    updated_date = models.DateTimeField(auto_now=True)
+    sub_headline = models.TextField()
+    news_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    upvotes = models.ManyToManyField(User, related_name='upvoted', blank=True)
+    downvotes = models.ManyToManyField(User, related_name='downvoted', blank=True)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.title
+
+    # def number_of_upvotes(self):
+    #     return self.upvotes.count()
+
+    # def number_of_downvotes(self):
+    #     return self.downvotes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    comment_upvote = models.ManyToManyField(User, related_name='comment_upvoted', blank=True)
+    comment_downvote = models.ManyToManyField(User, related_name='comment_downvoted', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.user_name}"
+
+    def number_of_upvotes(self):
+        return self.upvotes.count()
+
+    def number_of_downvotes(self):
+        return self.downvotes.count()
