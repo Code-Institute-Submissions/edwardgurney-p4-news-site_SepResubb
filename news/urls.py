07e06451aff6712e4550c45fa9f8bdfaf7a72986
edwardgurney <edwardgurney1979@gmail.com>
2021-12-30 12:58:16 +0000

    Add comments form and semi functionality to render back to site. Needs more work as not working properly

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView, ArticleDetailView
+from .views import HomeView, ArticleDetailView, submit_comment
 
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('article/<slug>', ArticleDetailView.as_view(), name='article-detail'),
+    path('comment/<slug>', submit_comment, name='submit-comment'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Comment
+from news.forms import CommentForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 
 
@@ -12,4 +17,17 @@ class HomeView(ListView):
 
 class ArticleDetailView(DetailView):
     model = Post
+    form = CommentForm
     template_name = 'article_details.html'
+
+@login_required
+def submit_comment(request, slug):
+    if request.method == "POST":
+        #form = CommentForm(request.POST)
+        user_comment = request.POST.get('body')
+        comment_post = get_object_or_404(Post, slug=slug)
+        user =  request.user
+        user_email = user.email
+        user_username = user.username
+        new_comment = Comment.objects.create(post =comment_post, email = user_email, user_name = user_username, body = user_comment)
+        return HttpResponseRedirect(reverse('article-detail', args=(comment_post.slug,)))
